f287d88f0b3baabedfff63bc637eb11279758c98
Albert Utarbaev <autardaev58@mail.ru>
2022-02-13 22:42:41 +0500

    11.02

--- Profile/index.php
+++ Profile/index.php
@@ -7,6 +7,83 @@ if ($a["hours"] >= 8 and $a["hours"] < 20) {
 else {
     $style = "style/styleTimeNight.css";
 }
+
+$textAboutMe = "Немного обо мне:<br>
+Я студент 2 курса МГТУ<br>
+Только начал учится программированию.<br>
+В прошлом немного знаком с языками C++ и Python.";
+
+$textAboutMe = explode("<br>", $textAboutMe);
+$textAboutMe[1] = [$textAboutMe[1], $textAboutMe[2], $textAboutMe[3]];
+$textAboutMe[1] = implode("<br>", $textAboutMe[1]);
+
+$textWishes = "
+На занятиях мне очень понравилась лояльность преподавателей.<br> 
+Практически любой вопрос, возникший на занятии,<br> 
+можно задать и получить на него понятный ответ.";
+
+$textWishes = explode(' ', $textWishes);
+
+$text = [];
+$text["Name"] = "Утарбаев Альберт";
+$text["aboutMe"] = "Немного обо мне: 
+Я студент 2 курса МГТУ 
+Только начал учится программированию. 
+В прошлом немного знаком с языками C++ и Python.";
+
+$text["wishes"] = "На занятиях мне очень понравилась лояльность преподавателей. 
+Практически любой вопрос, возникший на занятии, 
+можно задать и получить на него понятный ответ";
+
+$text["namePictures"] = [
+        "Не ешь, подумой!", "Не дали рыбовов!", 'Месье "Кот в сапоге"', "Толстощёк",
+        "Белый песик", "Моё одеяло!", "Пёс. Просто пёс.", "Пёсик сидит."
+];
+
+$text["footer"] = "©Все права защищены";
+
+$vowelsList = ["а", "А", "о", "О", "э", "Э", "е", "Е", "и", "И", "ы", "Ы", "у", "У", "ё", "Ё", "ю", "Ю", "я", "Я"];
+
+$countVowels = 0; //тут будут хранится количество гласных
+foreach ($text as $key => $value)
+{
+    if ($key != "namePictures")
+    {
+        for ($i = 0; $i < strlen($value); $i++)
+        {
+            $char = mb_substr($value, $i, 1);
+            if (in_array($char, $vowelsList))
+                $countVowels++;
+        }
+    }
+    else{
+        foreach ($text[$key] as $value)
+        {
+            for ($i = 0; $i < strlen($value); $i++)
+            {
+                $char = mb_substr($value, $i, 1);
+                if (in_array($char, $vowelsList))
+                    $countVowels++;
+            }
+        }
+    }
+}
+
+$countWords = 0;
+foreach ($text as $key => $value)
+{
+    if ($key != "namePictures")
+    {
+        $countWords += count(explode(' ', $value));
+    }
+    else{
+        foreach ($text[$key] as $value)
+        {
+            $countWords += count(explode(' ', $value));
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -25,15 +102,19 @@ else {
         <h2 class="name_profile">Утарбаев Альберт</h2>
         <div class="info">
             <p class="info-aboutMe">
-                Немного обо мне:<br>
-                Я студент 2 курса МГТУ<br>
-                Только начал учится программированию.<br>
-                В прошлом немного знаком с языками C++ и Python.
+                <?
+                echo "<span style='color: yellowgreen;'>$textAboutMe[0]<br></span>" . $textAboutMe[1];
+                ?>
             </p>
             <p class="info-wishes">
-                На занятиях мне очен понравилась лояльность преподавателей.<br>
-                Практически любой вопрос, возникший на занятии,<br>
-                можно задать и получить на него понятный ответ.<br>
+                <?
+                for ($i = 0; $i < count($textWishes); $i++) {
+                    if ($i % 2 == 0)
+                        echo "<span style='color: yellowgreen'>$textWishes[$i] </span>";
+                    else
+                        echo "<span style='color: yellow'>$textWishes[$i] </span>";
+                }
+                ?>
             </p>
         </div>
     </div>
@@ -76,6 +157,59 @@ else {
         </div>
     </div>
 </div>
+
+<div class="container-data">
+    <form method="get">
+        <fieldset class="data">
+            <legend>Дата рождения</legend>
+            <p>Год -
+                <select name="year">
+                    <?
+                    for ($i = 1975; $i <= 2022; $i++)
+                        echo "<option>$i</option>"
+                    ?>
+                </select>
+            </p>
+
+            <p>
+                Месяц -
+                <select name="month">
+                    <?
+                    for ($i = 1; $i <= 12; $i++)
+                        echo "<option>$i</option>"
+                    ?>
+                </select>
+            </p>
+
+            <p>
+                Дня -
+                <select name="day">
+                    <?
+                    for ($i = 1; $i <= 31; $i++)
+                        echo "<option>$i</option>"
+                    ?>
+                </select>
+            </p>
+            <input type="submit" name="submit">
+
+            <p>
+                <?
+                    $submit = $_GET["submit"];
+                    if(isset($submit))
+                    {
+                        $year = $_GET['year'];
+                        $month = $_GET['month'];
+                        $day = $_GET['day'];
+                        $strData = "$year-$month-$day";
+                        $days = (int)((time() - strtotime($strData)) / 86400);
+                        echo "$days - количество дней прошедших с вашего рождения." . "<br>";
+                    }
+                ?>
+            </p>
+        </fieldset>
+    </form>
+</div>
+
 <?php require_once "php/Footer.php"?>
 </body>
 </html>
